a0d724725c7f29dbb9c4b6e7e0046488d4872abc
JamesSama <cj31defita@gmail.com>
2023-09-30 18:19:12 +0800

    Sign Up Page
    made some adjustments, closer to ui design.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div id="logo-container" class="row justify-content-end">
     <img class="logo" src="https://scontent.xx.fbcdn.net/v/t1.15752-9/379654352_3562946250609323_738295434293063444_n.png?_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGZMv6FAu96ucIw9FgvM59oqD9CK9Li2FGoP0Ir0uLYUVw5RXDYxUx40zB5f911DRqtd30CW7ktyufKP8xR8UFV&_nc_ohc=GmJHtwpZdSoAX_3y2hK&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRHY3iuRBz5DrTaBwCTHK_usqfWm3alRCFvZAgC8sm6RA&oe=653B6BD6">
-        <div class="col-md-5">
+    <div class="col-md-5">
             <div class="card" id="card-marg">
-                <div id="card-hdr"><p class = "hdr"> Register <p></div>
+                <div id="card-hdr"><p class = "hdr"> Sign Up <p></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -16,7 +16,7 @@
                             <label for="name" id="textbox" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input placeholder="Enter Name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -30,7 +30,7 @@
                             <label for="email" id="textbox" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input placeholder="Enter Email Address" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -44,7 +44,7 @@
                             <label for="password" id="textbox" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input placeholder="Create Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -58,7 +58,7 @@
                             <label for="password-confirm" id="textbox" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input placeholder="Re-type Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
