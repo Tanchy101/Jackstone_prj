ecf14c5b7c45ac7ee6e6d00d8cc91c6293309a1a
Tanchyy <angelo.tancioco@gmail.com>
2023-10-11 00:21:28 +0800

    added side menu in admin page

--- resources/views/admin/adminIndex.blade.php
+++ resources/views/admin/adminIndex.blade.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+@extends('admin.adminSideBar')
 
-    <title>Admin Larastore</title>
-</head>
-<body>
-    <div class="card-header">
-        This is where the admin will redirect after logging in</div>
-    <div class="card-body">
-       <p class="card-text"> Dashboard siguro lalagay dito basta CRUD andito </p>
+@section('sidenav')
 
+<link rel="stylesheet" href="{{ asset('css/adminSideMenu.css') }}">
 
-      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-          {{ __('Logout')}}
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-          @crsf
-      </form>
-    </div>
-    </div>
-</body>
-</html>
-
+    
+    
+@endsection
---
+++ resources/views/admin/adminSideBar.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <title>Admin Larastore</title>
+</head>
+<body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <div class = "side-menu">
+        <div class = "brand-name">
+            <h2 class = "brand">Larastore</h2>
+        <div>
+        <ul>
+            <li>
+                <a href = "#">Dashboard</a>
+            </li>
+            <li>
+                <a href = "#">Sales Report</a>
+            </li>
+            <li>
+                <a href = "#">Orders</a>
+            </li>
+            <li>
+                <a href = "#">Profile</a>
+            </li>
+        </ul>
+
+    </div>
+        
+         
+         
+       
+
+
+
+
+
+
+
+    <main>
+        @yield('sidenav')
+    </main>
+
+
+
+</body>
+</html>
